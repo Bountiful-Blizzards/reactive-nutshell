18df4afa510d4516a77ec6700ab1c149741eeb72
Corri Golden <corri.golden@gmail.com>
2019-12-04 09:25:49 -0600

    update

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,9 @@
 import { Route } from "react-router-dom";
 import React, { Component } from "react";
 import MessageList from "./Messages/MessageList";
-<<<<<<< HEAD
 import Newslist from "./News/NewsList";
-=======
 import EventList from "./Events/EventList";
 import TaskList from "./Tasks/TaskList";
->>>>>>> master
 
 
 
