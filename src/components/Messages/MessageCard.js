e8e4793b6f531bab45b5ef4455bde0db957073ec
Corri Golden <corri.golden@gmail.com>
2019-12-04 09:55:18 -0600

    dynamically populated

--- src/components/Messages/MessageCard.js
+++ src/components/Messages/MessageCard.js
@@ -6,13 +6,13 @@ class MessageCard extends Component {
     render() {
         return (
 
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Message</h5>
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Message</h5>
                 <h2><span className="card-message">{this.props.message.message}</span></h2>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="card-link">Delete Message</a>
-                <a href="#" class="card-link">Edit Message</a>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="card-link">Delete Message</a>
+                <a href="#" className="card-link">Edit Message</a>
             </div>
         </div>
         )
