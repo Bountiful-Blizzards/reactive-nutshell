26c8401c961a996ed4eedf80b3bfdc8b8f19e624
Matthew Blagg <matthewscottblagg@gmail.com>
2019-12-04 10:59:54 -0600

    continuing adding new

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -18,49 +18,42 @@ export default class ApplicationViews extends Component {
         <Route
           exact path="/" render={props => {
             return null
-            // Remove null and return the component which will show news articles
           }}
         />
 
         <Route
           exact path="/register" render={props => {
             return null
-            // Remove null and return the component which will handle user registration
           }}
         />
 
         <Route
           path="/friends" render={props => {
             return null
-            // Remove null and return the component which will show list of friends
           }}
         />
 
         <Route
           path="/messages" render={props => {
             return <MessageList {...props} />
-            // Remove null and return the component which will show the messages
           }}
         />
 
         <Route
           exact path="/tasks" render={props => {
             return <TaskList {...props} />
-            // Remove null and return the component which will show the user's tasks
           }}
         />
 
         <Route
           path="/events" render={props => {
             return <EventList {...props} />
-            // Remove null and return the component which will show the user's events
           }}
         />
 
         <Route
           path="/articles" render={props => {
             return <Newslist {...props} />
-            // Remove null and return the component which will show the messages
           }}
         />
 
--- src/components/Tasks/TaskCard.js
+++ src/components/Tasks/TaskCard.js
@@ -5,24 +5,14 @@ import { Link } from "react-router-dom"
 class TaskCard extends Component {
     render() {
         return (
-<<<<<<< HEAD
-
-        <div class="card">
-            <div className="card-body">
-                <h5 className="card-title" style={{width: "18rem"}}><b>{this.props.task.task}</b></h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="card-link">Delete</a>
-                <a href="#" className="card-link">Edit</a>
-=======
             <div className="card">
                 <div className="card-body">
                     <input type="checkbox"></input>
-                    <Link to={`/animals/${this.props.task.id}`} className="card-title" style={{ width: "18rem" }}><h5><b>{this.props.task.task}</b></h5></Link>
+                    <Link to={`/tasks/${this.props.task.id}`} className="card-title" style={{ width: "18rem" }}><h5><b>{this.props.task.task}</b></h5></Link>
                     <div className="input-group-append" id="button-addon4">
                         <button className="btn btn-outline-secondary" type="button">Delete</button>
                     </div>
                 </div>
->>>>>>> master
             </div>
         )
     }
