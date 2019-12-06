46e0ea25f0e8a3fbeb3a8712e38f0d04247d56df
joseph walker <josephwalker504@gmail.com>
2019-12-06 09:20:32 -0600

    Added timestamp to code

--- src/components/News/NewsCard.js
+++ src/components/News/NewsCard.js
@@ -11,6 +11,7 @@ class NewsCard extends Component {
                     <h5 onClick={() => {this.props.history.push(`/articles/${this.props.article.id}/edit`)}} className="card-title" style={{ width: "18rem" }}><b>{this.props.article.title}</b></h5>
                     <div className="input-group-append" id="button-addon4">
                         <button type="button" onClick={() => this.props.deleteArticle(this.props.article.id)} >Delete</button>
+                        <h6>Time Stamp: {this.props.article.timeStamp}</h6>
                     </div>
                 </div>
             </div>
